ad64541640898ed0e71af825e3d5109044d32b3d
choewy <choewy32@gmail.com>
2022-04-08 00:26:59 +0900

    gitbook

---
+++ index.md
@@ -0,0 +1,7 @@
+---
+title: Why Jekyll with GitBook
+author: 최원영
+date: 2019-04-27
+layout: home
+---
+
--- server/bin/www.js
+++
@@ -1,6 +0,0 @@
-const server = require('../app');
-const port = 5000;
-
-server.listen(port, () => {
-    console.log(`Socker IO Server running on port ${port}`);
-});
